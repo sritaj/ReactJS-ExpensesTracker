372b94cc4931e8a2c60949cc5e68eaa1cf4aa9af
Sritaj <sritajp@gmail.com>
2021-05-19 00:56:06 +0530

    Added Expense Items into HTML structure and passed the Date props

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./ExpenseItems.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItems = (props) => {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{month} </div>
-      <div>{day}</div>
-      <div>{year}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.expense}</h2>
         <div className="expense-item__price">{props.price}</div>
