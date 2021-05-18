8b0bfa16e7acdae122a8b0b46db1d3a2e2633021
Sritaj <sritajp@gmail.com>
2021-05-19 00:23:37 +0530

    Added divs to represent Month Day Year and added Helper function to populate that

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./ExpenseItems.css";
 
 const ExpenseItems = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{month} </div>
+      <div>{day}</div>
+      <div>{year}</div>
       <div className="expense-item__description">
         <h2>{props.expense}</h2>
         <div className="expense-item__price">{props.price}</div>
