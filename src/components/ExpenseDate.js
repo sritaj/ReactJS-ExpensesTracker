41b91785260e0de8b007f3ffab8bce0884a56625
Sritaj <sritajp@gmail.com>
2021-05-19 01:01:04 +0530

    Imported ExpenseItems.css and added classNames

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,3 +1,4 @@
+import "./ExpenseDate.css";
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
@@ -5,10 +6,10 @@ function ExpenseDate(props) {
   console.log(props.date.getDate());
 
   return (
-    <div>
-      <div>{month} </div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month} </div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
