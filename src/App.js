80cba25b8838746aa8475bac3daaf134e9470662
Sritaj <sritajp@gmail.com>
2021-05-19 00:16:21 +0530

    Modified the App.JS to display ExpenseItems, added const expenseItems to pass value to fields defined in ExpenseItems.js via Props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expenseDetails = [
+    {
+      id: 1,
+      date: new Date(2021, 2, 10),
+      expense: "Internet Bills",
+      price: "15$",
+    },
+    {
+      id: 1,
+      date: new Date(2021, 2, 10),
+      expense: "Food Bills",
+      price: "150$",
+    },
+    {
+      id: 1,
+      date: new Date(2021, 2, 10),
+      expense: "Electricity Bills",
+      price: "8$",
+    },
+    {
+      id: 1,
+      date: new Date(2021, 2, 10),
+      expense: "Phone Bills",
+      price: "4$",
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ExpenseItems
+          date={expenseDetails[0].date}
+          expense={expenseDetails[0].expense}
+          price={expenseDetails[0].price}
+        />
+        <ExpenseItems
+          date={expenseDetails[1].date}
+          expense={expenseDetails[1].expense}
+          price={expenseDetails[1].price}
+        />
+        <ExpenseItems
+          date={expenseDetails[2].date}
+          expense={expenseDetails[2].expense}
+          price={expenseDetails[2].price}
+        />
+        <ExpenseItems
+          date={expenseDetails[3].date}
+          expense={expenseDetails[3].expense}
+          price={expenseDetails[3].price}
+        />
       </header>
     </div>
   );
