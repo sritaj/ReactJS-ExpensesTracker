13a65eee75bf192ceff407effc6d982f88d3eae8
Sritaj <sritajp@gmail.com>
2021-05-19 20:46:19 +0530

    Added Expenses.js to create separate component to handle Expense Items list, made changes in the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItems from "./components/ExpenseItems";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenseDetails = [
@@ -29,30 +29,12 @@ function App() {
       price: "4$",
     },
   ];
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Let's Get Started</h1>
-        <ExpenseItems
-          date={expenseDetails[0].date}
-          expense={expenseDetails[0].expense}
-          price={expenseDetails[0].price}
-        />
-        <ExpenseItems
-          date={expenseDetails[1].date}
-          expense={expenseDetails[1].expense}
-          price={expenseDetails[1].price}
-        />
-        <ExpenseItems
-          date={expenseDetails[2].date}
-          expense={expenseDetails[2].expense}
-          price={expenseDetails[2].price}
-        />
-        <ExpenseItems
-          date={expenseDetails[3].date}
-          expense={expenseDetails[3].expense}
-          price={expenseDetails[3].price}
-        />
+        <Expenses expenseDetails={expenseDetails} />
       </header>
     </div>
   );
---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import "./Expenses.css";
+import ExpenseItems from "./ExpenseItems";
+
+const Expenses = (props) => {
+  return (
+    <div>
+      <ExpenseItems
+        date={props.expenseDetails[0].date}
+        expense={props.expenseDetails[0].expense}
+        price={props.expenseDetails[0].price}
+      />
+      <ExpenseItems
+        date={props.expenseDetails[1].date}
+        expense={props.expenseDetails[1].expense}
+        price={props.expenseDetails[1].price}
+      />
+      <ExpenseItems
+        date={props.expenseDetails[2].date}
+        expense={props.expenseDetails[2].expense}
+        price={props.expenseDetails[2].price}
+      />
+      <ExpenseItems
+        date={props.expenseDetails[3].date}
+        expense={props.expenseDetails[3].expense}
+        price={props.expenseDetails[3].price}
+      />
+    </div>
+  );
+};
+
+export default Expenses;
