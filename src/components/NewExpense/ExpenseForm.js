f2793bc1526c6158b726d346a3658039ca962730
Sritaj <sritajp@gmail.com>
2021-05-21 19:37:57 +0530

    Added props to ExpenseForm, called the props.onSaveExpenseData and passed the ExpenseData object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
 
   const titleChangeHandler = (event) => {
@@ -29,6 +29,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setenteredDate("");
