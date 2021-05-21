d999fea2c1dcfcbe60c53f8121992e77b12c5799
Sritaj <sritajp@gmail.com>
2021-05-22 01:56:33 +0530

    Added $ before the props.amount

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -9,7 +9,7 @@ const ExpenseItems = (props) => {
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.price}</div>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </Card>
   );
