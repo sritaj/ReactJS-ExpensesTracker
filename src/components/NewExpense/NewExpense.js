b4fbee4c54c8400647d1dbadc3e7988b077ed640
Sritaj <sritajp@gmail.com>
2021-05-21 19:37:19 +0530

    Defined onSaveExpenseData on custom Component to be passed as a Child to Expense form, added the SaveExpenseHandler which accepts the data for the inputs designed in the ExpenseForm along with a Random Id

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,15 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString,
+    };
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
