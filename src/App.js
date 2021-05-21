77140e3bb308e56a9f41cbe7dae5d012ed771b45
Sritaj <sritajp@gmail.com>
2021-05-22 01:58:24 +0530

    Moved hardcoded dummy array list out of the function, added useState to check for expenses, added setExpenses with callback in Handler function to add the dynamic data

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import ExpenseForm from "./components/NewExpense/NewExpense";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    date: new Date(2021, 2, 10),
+    title: "Internet Bills",
+    price: "15",
+  },
+  {
+    id: 1,
+    date: new Date(2021, 2, 10),
+    title: "Food Bills",
+    price: "150",
+  },
+  {
+    id: 1,
+    date: new Date(2021, 2, 10),
+    title: "Electricity Bills",
+    price: "8",
+  },
+  {
+    id: 1,
+    date: new Date(2021, 2, 10),
+    title: "Phone Bills",
+    price: "4",
+  },
+];
 function App() {
-  const expenseDetails = [
-    {
-      id: 1,
-      date: new Date(2021, 2, 10),
-      title: "Internet Bills",
-      price: "15$",
-    },
-    {
-      id: 1,
-      date: new Date(2021, 2, 10),
-      title: "Food Bills",
-      price: "150$",
-    },
-    {
-      id: 1,
-      date: new Date(2021, 2, 10),
-      title: "Electricity Bills",
-      price: "8$",
-    },
-    {
-      id: 1,
-      date: new Date(2021, 2, 10),
-      title: "Phone Bills",
-      price: "4$",
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <NewExpense onAddExpense={addExpenseHandler} />
-        <Expenses expenseDetails={expenseDetails} />
+        <Expenses expenseDetails={expenses} />
       </header>
     </div>
   );
