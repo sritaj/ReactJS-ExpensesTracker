7b5b3003a60af1bbadd9336af84eff482f7b07d3
Sritaj <sritajp@gmail.com>
2021-05-21 11:41:17 +0530

    Designed the form in ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,26 @@
 import "./ExpenseForm.css";
-const ExpenseForm = () => {};
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense_controls">
+        <div className="new-expense_control">
+          <label>Title</label>
+          <input type="text"></input>
+        </div>
+        <div className="new-expense_control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01"></input>
+        </div>
+        <div className="new-expense_control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" step="2022-12-31"></input>
+        </div>
+      </div>
+      <div className="new-expense_actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
 
 export default ExpenseForm;
