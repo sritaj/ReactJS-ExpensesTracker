cdda6c4864b8a28c9a30a442a2a3a5dc3ae7ce41
Sritaj <sritajp@gmail.com>
2021-05-21 21:28:07 +0530

    Removed sample code of Button from ExpenseItems.js

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -4,19 +4,12 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItems = (props) => {
-  const [expense, setExpense] = useState(props.expense);
-
-  const clickHandler = () => {
-    setExpense("Updated!");
-    console.log(expense);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expense}</h2>
+        <h2>{props.expense}</h2>
         <div className="expense-item__price">{props.price}</div>
-        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
