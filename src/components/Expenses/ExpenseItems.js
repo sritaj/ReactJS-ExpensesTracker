b186c1d21455ce6769cb2d150dbf00c12a8f5b3c
Sritaj <sritajp@gmail.com>
2021-05-21 10:32:53 +0530

    Added button w.r.t to all Expense items and a sample onClick event

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -4,12 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItems = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.expense}</h2>
         <div className="expense-item__price">{props.price}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
