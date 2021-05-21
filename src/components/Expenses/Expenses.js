80035ec0a37a51f640f7c9243e2d156566732dca
Sritaj <sritajp@gmail.com>
2021-05-22 03:08:02 +0530

    Seggregated Filter logic in separate js files and added relevant css, updated divs to li in Expenses and used the ExpensesList custom component to render the Expenses using props

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import ExpenseItems from "./ExpenseItems";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -16,29 +17,16 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let filteredExpensesContent = <p>No Expenses found for the selected year </p>;
-
-  if (filteredExpenses.length > 0) {
-    filteredExpensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItems
-        key={expense.id}
-        title={expense.title}
-        date={expense.date}
-        price={expense.price}
-      />
-    ));
-  }
-
   return (
-    <div>
+    <li>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onYearFilter={filterChangeHandler}
         />
-        {filteredExpensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
-    </div>
+    </li>
   );
 };
 
