9e56207f26716d23e296026096c5f2e707b4bf6c
Sritaj <sritajp@gmail.com>
2021-05-21 11:04:19 +0530

    Added State Handler and added onClick func to change the Expense title based on button click

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItems = (props) => {
+  const [expense, setExpense] = useState(props.expense);
+
   const clickHandler = () => {
-    console.log("Clicked");
+    setExpense("Updated!");
+    console.log(expense);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.expense}</h2>
+        <h2>{expense}</h2>
         <div className="expense-item__price">{props.price}</div>
         <button onClick={clickHandler}>Change Title</button>
       </div>
